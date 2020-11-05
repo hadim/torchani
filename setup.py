72b21e4e838138180ef4d2a71bd8fbe766b6296b
Hadrien Mary <hadrien.mary@gmail.com>
2020-11-04 22:13:38 -0500

    build cuda extension in the main setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,64 @@
+import importlib
 from setuptools import setup, find_packages
+
+ext_modules = []
+cmdclass = {}
+
+# Check whether torch is available
+if importlib.util.find_spec("torch") is not None:
+    import torch
+
+    # Check whether Cuda is available
+    if torch.cuda.is_available():
+
+        from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+        nvcc_args = [
+            "-gencode=arch=compute_50,code=sm_50",
+            "-gencode=arch=compute_60,code=sm_60",
+            "-gencode=arch=compute_61,code=sm_61",
+            "-gencode=arch=compute_70,code=sm_70",
+            "-Xptxas=-v",
+            "--expt-extended-lambda",
+            "-use_fast_math",
+        ]
+
+        cuda_version = float(torch.version.cuda)
+        if cuda_version >= 10:
+            nvcc_args.append("-gencode=arch=compute_75,code=sm_75")
+        if cuda_version >= 11:
+            nvcc_args.append("-gencode=arch=compute_80,code=sm_80")
+        if cuda_version >= 11.1:
+            nvcc_args.append("-gencode=arch=compute_86,code=sm_86")
+
+        ext_modules = [
+            CUDAExtension(
+                name="cuaev",
+                sources=["torchani/extension/aev.cu"],
+                extra_compile_args={"cxx": ["-std=c++14"], "nvcc": nvcc_args},
+            )
+        ]
+
+        cmdclass = {"build_ext": BuildExtension}
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup_attrs = {
-    'name': 'torchani',
-    'description': 'PyTorch implementation of ANI',
-    'long_description': long_description,
-    'long_description_content_type': "text/markdown",
-    'url': 'https://github.com/aiqm/torchani',
-    'author': 'Xiang Gao',
-    'author_email': 'qasdfgtyuiop@gmail.com',
-    'license': 'MIT',
-    'packages': find_packages(),
-    'include_package_data': True,
-    'use_scm_version': True,
-    'setup_requires': ['setuptools_scm'],
-    'install_requires': [
-        'torch',
-        'lark-parser',
-        'requests'
-    ],
-}
-
-setup(**setup_attrs)
+setup(
+    name="torchani",
+    description="PyTorch implementation of ANI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aiqm/torchani",
+    author="Xiang Gao",
+    author_email="qasdfgtyuiop@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    install_requires=["torch", "lark-parser", "requests"],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+)
--- torchani/extension/setup.py
+++ torchani/extension/setup.py
@@ -7,6 +7,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 nvcc_args = ["-gencode=arch=compute_50,code=sm_50", "-gencode=arch=compute_60,code=sm_60",
              "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
              "-Xptxas=-v", '--expt-extended-lambda', '-use_fast_math']
+
 include_dirs = []
 cuda_version = float(torch.version.cuda)
 if cuda_version >= 10:
@@ -15,22 +16,6 @@ if cuda_version >= 11:
     nvcc_args.append("-gencode=arch=compute_80,code=sm_80")
 if cuda_version >= 11.1:
     nvcc_args.append("-gencode=arch=compute_86,code=sm_86")
-if cuda_version < 11:
-    # if no cub, download it to include dir from github
-    if not os.path.isdir('./include/cub'):
-        if not os.path.exists('./include'):
-            os.makedirs('include')
-        commands = """
-        echo "Downloading CUB library";
-        wget -q https://github.com/NVIDIA/cub/archive/main.zip;
-        unzip -q main.zip -d include;
-        mv include/cub-main/cub include;
-        echo "Removing unnecessary files";
-        rm main.zip;
-        rm -rf include/cub-main;
-        """
-        subprocess.run(commands, shell=True, check=True, universal_newlines=True)
-    include_dirs.append(os.path.abspath("./include"))
 
 setup(
     name='cuaev',
